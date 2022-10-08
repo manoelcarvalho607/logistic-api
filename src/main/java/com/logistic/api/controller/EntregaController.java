5f4aed628d96861d87cb447df362bc1f8aa5bc1c
Manoel Carvalho <manoelcarvalho1984@gmail.com>
2022-10-07 23:29:30 -0300

    isolando Domain Model do Representation Model

--- src/main/java/com/logistic/api/controller/EntregaController.java
+++ src/main/java/com/logistic/api/controller/EntregaController.java
@@ -21,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.logistic.api.domain.model.Entrega;
 import com.logistic.api.domain.repository.EntregaRepository;
 import com.logistic.api.domain.service.SolicitacaoEntregaService;
+import com.logistic.api.model.DestinatarioModel;
+import com.logistic.api.model.EntregaModel;
 
 /**
  * @author manoel.carvalho
@@ -49,9 +51,25 @@ public class EntregaController {
 	}
 	
 	@GetMapping("/{entregaId}")
-	public ResponseEntity<Entrega> buscar(@PathVariable Long entregaId) {
+	public ResponseEntity<EntregaModel> buscar(@PathVariable Long entregaId) {
 		return entregaRepository.findById(entregaId)
-				.map(ResponseEntity::ok)
+				.map(entrega -> {
+					EntregaModel entregaModel = new EntregaModel();
+					entregaModel.setId(entrega.getId());
+					entregaModel.setNomeCliente(entrega.getCliente().getNome());
+					entregaModel.setDestinatario(new DestinatarioModel());
+					entregaModel.getDestinatario().setNome(entrega.getDestinatario().getNome());
+					entregaModel.getDestinatario().setLogradouro(entrega.getDestinatario().getLogradouro());
+					entregaModel.getDestinatario().setNumero(entrega.getDestinatario().getNumero());
+					entregaModel.getDestinatario().setComplemento(entrega.getDestinatario().getComplemento());
+					entregaModel.getDestinatario().setBairro(entrega.getDestinatario().getBairro());
+					entregaModel.setTaxa(entrega.getTaxa());
+					entregaModel.setStatus(entrega.getStatus());
+					entregaModel.setDataPedido(entrega.getDataPedido());
+					entregaModel.setDataFinalizacao(entrega.getDataFinalizacao());
+					
+					return ResponseEntity.ok(entregaModel);
+				})
 				.orElse(ResponseEntity.notFound().build());
 	}
 }
